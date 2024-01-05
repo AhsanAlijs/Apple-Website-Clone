96bcd1c575216f6b3eb87d29648aa19f4dc5981c
Ahsan ali <120973691+AhsanAlijs@users.noreply.github.com>
2024-01-06 00:32:39 +0500

    navbar complete

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,31 @@ import images from './assets/Websiteimages/Navbar.png'
 function App() {
   return (
     <>
-    <div>
-      <div>
-        <img src={images} alt="" />
+      <div className='flex items-center justify-center bg-[#6a6969] gap-[10rem] '>
+        <div>
+          <img src={images} alt="new" className='w-[50px]' />
+        </div>
+        <nav className='flex items-center justify-center gap-[10px]'>
+
+          <ul className='flex items-center justify-center gap-[30px] text-[white]'>
+            <li><a>Store</a></li>
+            <li><a>Mac</a></li>
+            <li><a>ipad</a></li>
+            <li><a>iphone</a></li>
+            <li><a>Watch</a></li>
+            <li><a>vision</a></li>
+            <li><a>Airpods</a></li>
+            <li><a>TV&Home</a></li>
+            <li><a>Entertainment</a></li>
+            <li><a>Accessories</a></li>
+            <li><a>Support</a></li>
+          </ul>
+          <div className='text-[white] flex items-center justify-center gap-[10px]'>
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <i class="fa-solid fa-bag-shopping"></i>
+          </div>
+        </nav>
       </div>
-      <nav>
-
-        <ul>
-          <li><a>Store</a></li>
-          <li><a>Mac</a></li>
-          <li><a>ipad</a></li>
-          <li><a>iphone</a></li>
-          <li><a>Watch</a></li>
-          <li><a>vision</a></li>
-          <li><a>Airpods</a></li>
-          <li><a>TV&Home</a></li>
-          <li><a>Entertainment</a></li>
-          <li><a>Accessories</a></li>
-          <li><a>Support</a></li>
-        </ul>
-
-      </nav>
-    </div>
     </>
   )
 }
